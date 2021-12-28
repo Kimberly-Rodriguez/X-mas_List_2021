af5356aed68245476a5f308b5a515bcef0d9966d
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-12-28 10:21:28 -0800

    assigning value to id and elements from html to javascrip file

---
+++ script.js
@@ -0,0 +1,8 @@
+let groceryItemInput = document.querySelector("#grocery-item");
+let buttonToAddToList = document.getElementById("add-to-list");
+let needToBuyEl = document.querySelector("#need-to-buy");
+let boughtEl = document.querySelector("#bought");
+
+let itemsINeedToBuy = [];
+let itemsIBought = [];
+
