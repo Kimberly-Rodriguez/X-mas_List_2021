66fbc67849d255a549f5bb6708f69e752e706345
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-12-29 18:48:08 -0800

    adding eventlister + looping gift items and appending them onto the DOM

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let groceryItemInput = document.querySelector("#grocery-item");
+let giftItemInput = document.querySelector("#gift-item");
 let buttonToAddToList = document.getElementById("add-to-list");
 let needToBuyEl = document.querySelector("#need-to-buy");
 let boughtEl = document.querySelector("#bought");
@@ -6,3 +6,32 @@ let boughtEl = document.querySelector("#bought");
 let itemsINeedToBuy = [];
 let itemsIBought = [];
 
+//creting and event listener for the addToList button
+buttonToAddToList.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //getting the valuue from giftItemInput
+  let item = giftItemInput.value;
+
+  //pushing itemsINeedToBuy onto the array
+  itemsINeedToBuy.push(item);
+  console.log(itemsINeedToBuy);
+
+  //saving items on localstorage
+  localStorage.setItem("need-to-buy", JSON.stringify(itemsINeedToBuy));
+
+  // to eliminate duplicates
+  needToBuyEl.textContent = "";
+
+  //looping through the array of itemsINeedToBuy
+  for (let i = 0; i < itemsINeedToBuy.length; i++) {
+    console.log(itemsINeedToBuy[i]);
+
+    //element dinamically created
+    let listEl = document.createElement("li");
+    listEl.textContent = itemsINeedToBuy[i];
+
+    // add onto the DOM by appending
+    needToBuyEl.appendChild(listEl);
+  }
+});
