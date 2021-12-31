c375e924d9597fe280220313e2508b40219df890
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-12-31 14:51:11 -0800

    create a new function that renders things I need to buy and call it for organizational purposes

--- script.js
+++ script.js
@@ -6,6 +6,37 @@ let boughtEl = document.querySelector("#bought");
 let itemsINeedToBuy = [];
 let itemsIBought = [];
 
+
+
+function renderItemsINeedToBuy() {
+   // to eliminate duplicates
+   needToBuyEl.textContent = "";
+
+   //looping through the array of itemsINeedToBuy
+   for (let i = 0; i < itemsINeedToBuy.length; i++) {
+     //console.log(itemsINeedToBuy[i]);
+ 
+     //element dinamically created
+     let listEl = document.createElement("li");
+     listEl.textContent = itemsINeedToBuy[i];
+ 
+     //create a button
+     let boughtButton = document.createElement("button");
+     //giving the button text
+     boughtButton.textContent = "BOUGHT";
+     //giving the button a class
+     boughtButton.setAttribute("class", "btn-danger");
+     //giving the button a data attribute
+     boughtButton.setAttribute("data-index", i); 
+     //appending button to listEl 
+     listEl.appendChild(boughtButton);
+ 
+     // add onto the DOM by appending
+     needToBuyEl.appendChild(listEl);
+   }
+
+}
+
 //creting and event listener for the addToList button
 buttonToAddToList.addEventListener("click", function (e) {
   e.preventDefault();
@@ -20,29 +51,8 @@ buttonToAddToList.addEventListener("click", function (e) {
   //saving items on localstorage
   localStorage.setItem("need-to-buy", JSON.stringify(itemsINeedToBuy));
 
-  // to eliminate duplicates
-  needToBuyEl.textContent = "";
+  renderItemsINeedToBuy(); 
 
-  //looping through the array of itemsINeedToBuy
-  for (let i = 0; i < itemsINeedToBuy.length; i++) {
-    console.log(itemsINeedToBuy[i]);
-
-    //element dinamically created
-    let listEl = document.createElement("li");
-    listEl.textContent = itemsINeedToBuy[i];
-
-    //create a button
-    let boughtButton = document.createElement("button");
-    //giving the button text
-    boughtButton.textContent = "Bought";
-    //giving the button a class
-    boughtButton.setAttribute("class", "btn-danger");
-    //giving the button a data attribute
-    boughtButton.setAttribute("data-index", i); 
-    //appending button to listEl 
-    listEl.appendChild(boughtButton);
-
-    // add onto the DOM by appending
-    needToBuyEl.appendChild(listEl);
-  }
 });
+
+init();
